47041e8a5bc189a52eb6dbcb17b3cfe0e6c22f8e
pgvalle <pgvalle.nogueira@gmail.com>
2026-09-17 12:35:30 -0300

    fixed wrong rotation direction

--- src/point.c
+++ src/point.c
@@ -4,7 +4,9 @@
 #define PI 3.14159265358979323846
 
 point_t rotate_n_move_point(point_t pt, int deg, point_t off) {
-    double rad = (PI * deg) / 180.0;
+    // In math the Y axis goes up
+    // Here it goes down, so we gotta invert the sign
+    double rad = -(PI * deg) / 180.0;
     point_t out;
     out.x = round(pt.x * cos(rad) - pt.y * sin(rad)) + off.x;
     out.y = round(pt.x * sin(rad) + pt.y * cos(rad)) + off.y;
